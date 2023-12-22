f8a0436de7c089ce99a4944bee071ae0aebfd3fa
David Changea <14ir155@gmail.com>
2023-12-22 09:55:30 +0100

    ajout des images et des figcaption dynamiquement sur le dom

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,11 +1,25 @@
-//test swagger
+/* variables du projet */
+const gallery = document.querySelector(".gallery");
+
+/* test backend + récupération des works */
 async function getWorks() {
   const response = await fetch("http://localhost:5678/api/works");
-  const responseJson = await response.json();
-  console.log(responseJson);
+  return await response.json();
 }
 getWorks();
 
-//remove gallery et son contenue dans le index.html
-const removeGallery = document.querySelector("#portfolio .gallery");
-removeGallery.remove("#portfolio .gallery");
+async function showWorks() {
+  const arrayWorks = await getWorks();
+  console.log(arrayWorks);
+  for (i = 0; i < arrayWorks.length; i++) {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    gallery.appendChild(figure);
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    img.src = arrayWorks[i].imageUrl;
+    figcaption.innerHTML = arrayWorks[i].title;
+  }
+}
+showWorks();
